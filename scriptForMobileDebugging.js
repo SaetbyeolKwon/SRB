6d3ea23aa07fda5b5bf3a2c49539d678dbb034a7
Saetbyeol Kwon <43006719+SaetbyeolKwon@users.noreply.github.com>
2023-12-19 22:22:21 -0500

    mobiledebugging

--- scriptForMobileDebugging.js
+++ scriptForMobileDebugging.js
@@ -8,6 +8,8 @@ recognition.lang = 'en-US';
 let listening = false;
 let displayText = ''; // Original text
 
+console.log("Script loaded. SpeechRecognition available:", !!window.SpeechRecognition);
+
 document.addEventListener('DOMContentLoaded', loadTextFile);
 
 // Original code
@@ -56,8 +58,16 @@ recognition.addEventListener('result', e => {
     const results = Array.from(e.results);
     const isFinal = results[results.length - 1].isFinal;
     const spokenText = Array.from(e.results).map(result => result[0]).map(result => result.transcript).join('');
+
     console.log("Recognition result:", spokenText);
-    updateReadingPosition(spokenText);
+    console.log(`Is final result: ${isFinal}`);
+    
+    if (isFinal) {
+        console.log("Final result:", spokenText);
+        updateReadingPosition(spokenText);
+    } else {
+        console.log("Interim result:", spokenText);
+    }
 
     // Reset the timer on new voice input
     // clearTimeout(autoPauseTimer);
@@ -84,6 +94,25 @@ recognition.addEventListener('result', e => {
 //     }
 // }
 
+// When speech recognition ended unexpectedly.
+let recognitionEnd = true;
+
+recognition.addEventListener('end', () => {
+    console.log("Speech recognition service ended.");
+    if (listening && recognitionEnd) {
+        console.log("Speech recognition service ended unexpectedly, restarting...");
+        recognition.start();
+    }
+});
+
+recognition.addEventListener('start', () => {
+    console.log("Speech recognition started.");
+});
+
+recognition.addEventListener('stop', () => {
+    console.log("Speech recognition stopped.");
+});
+
 const startBtn = document.getElementById('start-btn');
 const pauseBtn = document.getElementById('pause-btn');
 let isPaused = false;
@@ -93,11 +122,14 @@ pauseBtn.addEventListener('click', togglePause);
 
 function toggleReading() {
     listening = !listening;
+    console.log("Toggle reading:", listening);
     if (listening) {
+        recognitionEnd = true;
         recognition.start();
         startBtn.textContent = 'Stop Reading';
         resumeAllSounds();
     } else {
+        recognitionEnd = false;
         recognition.stop();
         startBtn.textContent = 'Start Reading';
         stopAllSounds();
@@ -114,4 +146,6 @@ function togglePause() {
         pauseBtn.textContent = 'Pause';
     }
     isPaused = !isPaused;
+    console.log("Toggle pause:", isPaused);
+    recognitionEnd = false;
 }
